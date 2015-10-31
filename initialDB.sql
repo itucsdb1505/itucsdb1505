d7984d5938a87829da000808c01ffdc7f0c188c4
Emre EROĞLU <emreeroglu@itu.edu.tr>
2015-11-01 01:02:13 +0200

    initial sql file is created with example data. Note data is redundant !

---
+++ initialDB.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS test;
+CREATE TABLE test(num int);
+INSERT INTO test values(1);
+INSERT INTO test values(2);
+INSERT INTO test values(3);
+INSERT INTO test values(4);
+INSERT INTO test values(5);
