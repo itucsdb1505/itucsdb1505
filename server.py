8b3c82ac495e6d9e857c45f034364dcb58fa5bc4
Emre EROĞLU <emreeroglu@itu.edu.tr>
2015-11-03 01:09:07 +0200

    User Update Added Conflicts Resolved

--- server.py
+++ server.py
@@ -26,20 +26,47 @@ def userManagement():
     for user in userListAsTuple:
         userListAsList.append(list(user))
 
-    return render_template('userManagement.html', userList = userListAsList)
+    return render_template('userManagement.html', userList=userListAsList, user4Update=None)
 
 
-@app.route('/addUser' , methods = ['POST'])
+@app.route('/addUser' , methods=['POST'])
 def addUser():
     name = request.form['name']
     age = request.form['age']
     email = request.form['email']
     auth = request.form['auth']
     cursor = dataBaseSetup.connection.cursor()
-    query = """INSERT INTO USERS values('""" + name + """',""" +  age + """,'""" + email + """','""" + auth + """')"""
+    query = """INSERT INTO USERS values('""" + name + """',""" + age + """,'""" + email + """','""" + auth + """')"""
     cursor.execute(query)
     dataBaseSetup.connection.commit()
     return redirect('/userManagement')
+
+@app.route('/userUpdate', methods=['POST'])
+def userUpdate():
+    name = request.form['name']
+    age = request.form['age']
+    email = request.form['email']
+    auth = request.form['auth']
+    cursor = dataBaseSetup.connection.cursor()
+    query = """UPDATE users SET name='""" + name + """',age=""" + age + """,email='""" + email + """',auth='""" + auth + """' WHERE email='""" + email + """';"""
+    cursor.execute(query)
+    dataBaseSetup.connection.commit()
+    return redirect('/userManagement')
+
+@app.route('/updateUser', methods=['POST'])
+def updateUser():
+    cursor = dataBaseSetup.connection.cursor()
+    email = request.form['email']
+    query = """select name, age, email, auth from users where email='""" + email + """';"""
+    cursor.execute(query)
+    user4Update = list(cursor.fetchall()[0])
+
+    cursor.execute("""select name, age, email, auth from USERS;""")
+    userListAsTuple = cursor.fetchall()
+    userListAsList = []
+    for user in userListAsTuple:
+        userListAsList.append(list(user))
+    return render_template('userManagement.html', userList=userListAsList, user4Update=user4Update)
 
 @app.route('/initiateDB')
 def initiateDB():
@@ -47,7 +74,17 @@ def initiateDB():
     cursor = dataBaseSetup.connection.cursor()
     cursor.execute("""select * from test;""")
     now = datetime.datetime.now()
-    return render_template('initiateDB.html', current_time=now.ctime(), data = cursor.fetchall())
+    return render_template('initiateDB.html', current_time=now.ctime(), data=cursor.fetchall())
+
+@app.route('/Pools')
+def pool_list():
+    now = datetime.datetime.now()
+    return render_template('pools.html', current_time=now.ctime())
+
+@app.route('/AddPool')
+def pool_edit():
+    now = datetime.datetime.now()
+    return render_template('pool_edit.html', current_time=now.ctime())
 
 
 if __name__ == '__main__':
