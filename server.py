ec7361edd0f6c7559d072746e4135a0dc4191b80
Emin <ocake@itu.edu.tr>
2015-11-03 16:16:41 +0300

    countries and leagues sites are added

--- server.py
+++ server.py
@@ -161,6 +161,75 @@ def deletePlayer():
     dataBaseSetup.connection.commit()
     return redirect('/players')
 
+@app.route('/leagues')
+def leagues():
+    now = datetime.datetime.now()
+    cursor = dataBaseSetup.connection.cursor()
+    query="""select id, name, classification, nation from LEAGUES;"""
+    cursor.execute(query)
+    leagueListAsTuple = cursor.fetchall()
+    leagueListAsList = []
+    for league in leagueListAsTuple:
+        leagueListAsList.append(list(league))
+
+    return render_template('leagues.html', leagueList=leagueListAsList, current_time=now.ctime())
+
+
+@app.route('/addLeague' , methods = ['POST'])
+def addLeague():
+    id= request.form['id']
+    name = request.form['name']
+    classification = request.form['classification']
+    nation = request.form['nation']
+    cursor = dataBaseSetup.connection.cursor()
+    cursor.execute("INSERT INTO LEAGUES (id, name, classification, nation) VALUES (%s, %s, %s, %s)", (id, name, classification, nation))
+    dataBaseSetup.connection.commit()
+    return redirect('/leagues')
+
+
+@app.route('/deleteLeague' , methods=['POST'])
+def deleteLeague():
+    id = request.form['id']
+    cursor = dataBaseSetup.connection.cursor()
+    query = """DELETE FROM LEAGUES WHERE id=""" + id + """;"""
+    cursor.execute(query)
+    dataBaseSetup.connection.commit()
+    return redirect('/leagues')
+
+@app.route('/countries')
+def countries():
+    now = datetime.datetime.now()
+    cursor = dataBaseSetup.connection.cursor()
+    query="""select id, name, population from COUNTRIES;"""
+    cursor.execute(query)
+    countryListAsTuple = cursor.fetchall()
+    countryListAsList = []
+    for country in countryListAsTuple:
+        countryListAsList.append(list(country))
+
+    return render_template('countries.html', countryList=countryListAsList, current_time=now.ctime())
+
+
+@app.route('/addCountry' , methods = ['POST'])
+def addCountry():
+    id= request.form['id']
+    name = request.form['name']
+    population = request.form['population']
+    cursor = dataBaseSetup.connection.cursor()
+    cursor.execute("INSERT INTO COUNTRIES (id, name, population) VALUES (%s, %s, %s)", (id, name,population))
+    dataBaseSetup.connection.commit()
+    return redirect('/countries')
+
+
+@app.route('/deleteCountry' , methods=['POST'])
+def deleteCountry():
+    id = request.form['id']
+    cursor = dataBaseSetup.connection.cursor()
+    query = """DELETE FROM COUNTRIES WHERE id=""" + id + """;"""
+    cursor.execute(query)
+    dataBaseSetup.connection.commit()
+    return redirect('/countries')
+
 if __name__ == '__main__':
     try:
         VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
@@ -176,4 +245,3 @@ if __name__ == '__main__':
     except:
         print("Error in server setup. Exception: ")
         print(sys.exc_info()[0])
-
